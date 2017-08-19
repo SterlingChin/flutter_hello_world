08c87b0e0d405cda6d5ca625714e00d967a7f973
Sterling Chin <sterling.chin@gmail.com>
2017-08-18 23:21:20 -0600

    hello world in flutter

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    new Container(
+      decoration: new BoxDecoration(color: Colors.blue),
+      child: new Center(
+        child: new Text("Hello World!  This is my first Flutter App")
+      )
+    )
+  );
+}
